e9fea0c66ab30bc36861b7a52696bcbf03d705cd
Nikola <nvelinov@my.bcit.ca>
2021-11-27 15:01:35 -0800

    Commmit #1 - Created repository and completed functions for index page

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+var main = document.querySelector(".main");
+
+function cleanUpIndex() {
+    while (main.lastChild) {
+        main.removeChild(main.lastChild)
+    }
+}
+
+function createSingleIndex(object) {
+    var Link = document.createElement("a")
+    var NewDiv = document.createElement("div")
+
+    Link.href = "page3.html"
+    NewDiv.classList.add("contact")
+    NewDiv.innerHTML = object.name
+    Link.append(NewDiv)
+    return Link
+}
+
+function renderIndex(array) {
+    for (let index = 0; index < array.length; index++) {
+        let element = array[index]
+        main.append(createSingleIndex(element))
+        
+}
+
+}
