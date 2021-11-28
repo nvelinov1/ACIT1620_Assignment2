4d870f1fea25f81404f12662026d730e12966b11
Nikola <nvelinov@my.bcit.ca>
2021-11-27 17:34:58 -0800

    Commit #3 - Completed functions for create page and cleaned up code

--- script.js
+++ script.js
@@ -64,10 +64,8 @@ function renderView(object) {
     ContactPhoneDiv.classList.add("contactphone")
     ContactAddressDiv.classList.add("contactaddress")
     ButtonsDiv.classList.add("buttons")
-    EditButton.classList.add("button")
-    EditButton.classList.add("edit")
-    CloseButton.classList.add("button")
-    CloseButton.classList.add("close")
+    EditButton.classList.add("button", "edit")
+    CloseButton.classList.add("button", "close")
 
     ContactNameImg.src = "./img/profile.jpg"
     ContactNameImg.alt = "Profile Picture"
@@ -82,13 +80,100 @@ function renderView(object) {
     CloseButton.innerHTML = "Close"
 
     main.append(ContactInfoDiv)
-    ContactInfoDiv.append(ContactNameDiv)
-    ContactInfoDiv.append(ContactEmailDiv)
-    ContactInfoDiv.append(ContactPhoneDiv)
-    ContactInfoDiv.append(ContactAddressDiv)
-    ContactInfoDiv.append(ButtonsDiv)
-    ContactInfoDiv.append(ContactNameImg)
+    ContactInfoDiv.append(ContactNameDiv, ContactEmailDiv, ContactPhoneDiv, ContactAddressDiv, ButtonsDiv, ContactNameImg)
     ContactNameDiv.append(ContactNameImg)
-    ButtonsDiv.append(EditButton)
-    ButtonsDiv.append(CloseButton)
+    ButtonsDiv.append(EditButton, CloseButton)
+}
+
+
+function cleanUpCreate() {
+    while (main.lastChild) {
+        main.removeChild(main.lastChild)
+    }
+}
+
+function renderCreate() {
+    ContactEditDiv = document.createElement("div")
+    ContactImageDiv = document.createElement("div")
+    ContactImageImg = document.createElement("img")
+    FormDiv = document.createElement("div")
+    FormForm = document.createElement("form")
+    NameDiv = document.createElement("div")
+    NameInput = document.createElement("input")
+    ExtraNameButton = document.createElement("button")
+    PhoneDiv = document.createElement("div")
+    PhoneInput = document.createElement("input")
+    ExtraPhoneButton = document.createElement("button")
+    AddressDiv = document.createElement("div")
+    AddressInput = document.createElement("input")
+    ExtraAddressButton = document.createElement("button")
+    EmailDiv = document.createElement("div")
+    EmailInput = document.createElement("input")
+    ExtraEmailButton = document.createElement("button")
+    CreateButtonsDiv = document.createElement("div")
+    SubmitButton = document.createElement("button")
+    ResetButton = document.createElement("button")
+
+    ContactEditDiv.classList.add("contactedit")
+    ContactImageDiv.classList.add("contactimg")
+    ContactImageImg.classList.add("profilepic")
+    FormDiv.classList.add("form")
+    NameDiv.classList.add("inputcontainer")
+    ExtraNameButton.classList.add("extrafield")
+    PhoneDiv.classList.add("inputcontainer")
+    ExtraPhoneButton.classList.add("extrafield")
+    AddressDiv.classList.add("inputcontainer")
+    ExtraAddressButton.classList.add("extrafield")
+    EmailDiv.classList.add("inputcontainer")
+    ExtraEmailButton.classList.add("extrafield")
+    CreateButtonsDiv.classList.add("buttons")
+    SubmitButton.classList.add("button", "save")
+    ResetButton.classList.add("button", "cancel")
+
+    ContactImageImg.src = "./img/profile.jpg"
+    NameInput.type = "text"
+    NameInput.id = "contactname"
+    NameInput.name = "contactname"
+    NameInput.placeholder = "Contact Name"
+    ExtraNameButton.id = "extranamefield"
+    ExtraNameButton.name = "extranamefield"
+    PhoneInput.type = "tel"
+    PhoneInput.id = "contactphone"
+    PhoneInput.name = "contactphone"
+    PhoneInput.placeholder = "Contact Phone"
+    ExtraPhoneButton.id = "extraphonefield"
+    ExtraPhoneButton.name = "extraphonefieldd"
+    AddressInput.id = "contactaddress"
+    AddressInput.name = "contactaddress"
+    AddressInput.placeholder = "Contact Address"
+    ExtraAddressButton.id = "extraaddressfield"
+    ExtraAddressButton.name = "extraaddressfield"
+    EmailInput.id = "contactemail"
+    EmailInput.name = "contactemail"
+    EmailInput.placeholder = "Contact Email"
+    ExtraEmailButton.id = "extraemailfield"
+    ExtraEmailButton.name = "extraemailfield"
+    SubmitButton.type = "submit"
+    SubmitButton.id = "savecontact"
+    SubmitButton.name = "savecontact"
+    ResetButton.id = "cancel"
+    ResetButton.name = "cancel"
+
+    ExtraNameButton.innerHTML = "+"
+    ExtraPhoneButton.innerHTML = "+"
+    ExtraAddressButton.innerHTML = "+"
+    ExtraEmailButton.innerHTML = "+"
+    SubmitButton.innerHTML = "Save Contact"
+    ResetButton.innerHTML = "Cancel"
+    
+    main.append(ContactEditDiv)
+    ContactEditDiv.append(ContactImageDiv, FormDiv)
+    ContactImageDiv.append(ContactImageImg)
+    FormDiv.append(FormForm)
+    FormForm.append(NameDiv, PhoneDiv, AddressDiv, EmailDiv, CreateButtonsDiv)
+    NameDiv.append(NameInput, ExtraNameButton)
+    PhoneDiv.append(PhoneInput, ExtraPhoneButton)
+    AddressDiv.append(AddressInput, ExtraAddressButton)
+    EmailDiv.append(EmailInput, ExtraEmailButton)
+    CreateButtonsDiv.append(SubmitButton, ResetButton)
 }
