ce00e5c4e213ee631926f505b1394bf4858820aa
Nikola <nvelinov@my.bcit.ca>
2021-11-27 16:01:38 -0800

    Commit #2 - Completed functions for view page

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var main = document.querySelector(".main");
 
+var contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+]
+
 function cleanUpIndex() {
     while (main.lastChild) {
         main.removeChild(main.lastChild)
@@ -23,5 +38,57 @@ function renderIndex(array) {
         main.append(createSingleIndex(element))
         
 }
+}
 
+function cleanUpView() {
+    while (main.lastChild) {
+        main.removeChild(main.lastChild)
+    }
+}
+
+function renderView(object) {
+    var ContactInfoDiv = document.createElement("div")
+    var ContactNameDiv = document.createElement("div")
+    var ContactNameImg = document.createElement("img")
+    var ContactEmailDiv = document.createElement("div")
+    var ContactPhoneDiv = document.createElement("div")
+    var ContactAddressDiv = document.createElement("div")
+    var ButtonsDiv = document.createElement("div")
+    var EditButton = document.createElement("button")
+    var CloseButton = document.createElement("button")
+
+    ContactInfoDiv.classList.add("contactinfo")
+    ContactNameDiv.classList.add("contactname")
+    ContactNameImg.classList.add("profilepic") 
+    ContactEmailDiv.classList.add("contactemail")
+    ContactPhoneDiv.classList.add("contactphone")
+    ContactAddressDiv.classList.add("contactaddress")
+    ButtonsDiv.classList.add("buttons")
+    EditButton.classList.add("button")
+    EditButton.classList.add("edit")
+    CloseButton.classList.add("button")
+    CloseButton.classList.add("close")
+
+    ContactNameImg.src = "./img/profile.jpg"
+    ContactNameImg.alt = "Profile Picture"
+    EditButton.value = "Edit"
+    CloseButton.value = "Close"
+
+    ContactNameDiv.innerHTML = object.name
+    ContactEmailDiv.innerHTML = "email: " + object.email
+    ContactPhoneDiv.innerHTML = "cell: " + object.phone
+    ContactAddressDiv.innerHTML = "address: " + object.address
+    EditButton.innerHTML = "Edit"
+    CloseButton.innerHTML = "Close"
+
+    main.append(ContactInfoDiv)
+    ContactInfoDiv.append(ContactNameDiv)
+    ContactInfoDiv.append(ContactEmailDiv)
+    ContactInfoDiv.append(ContactPhoneDiv)
+    ContactInfoDiv.append(ContactAddressDiv)
+    ContactInfoDiv.append(ButtonsDiv)
+    ContactInfoDiv.append(ContactNameImg)
+    ContactNameDiv.append(ContactNameImg)
+    ButtonsDiv.append(EditButton)
+    ButtonsDiv.append(CloseButton)
 }
